dba596e6a2c014c0dab1462f84a0e8ed93e5664d
bhivgadearav <hazeifygameplay5927@gmail.com>
2025-12-16 13:28:20 +0530

    added prettier-config package & deleted ui package

---
+++ packages/prettier-config/index.js
@@ -0,0 +1,11 @@
+/**
+ * @type {import("prettier").Config}
+ */
+export default {
+  // Your team’s shared Prettier rules
+  tabWidth: 2,
+  singleQuote: true,
+  trailingComma: "all",
+  semi: false,
+  printWidth: 100,
+};
